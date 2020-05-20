b150a50eb5a01dd364b98f506e9450ecb7a364c9
Heidi Tran <HeidiTran1410@gmail.com>
2020-05-20 11:06:57 -0400

    Add Bootstrap

--- code-spot-client/src/app/app.module.ts
+++ code-spot-client/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
 
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
