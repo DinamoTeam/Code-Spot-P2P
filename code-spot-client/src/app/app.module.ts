358d742680e80b26d78a0f91e1d7e3d576fe37bb
Heidi Tran <HeidiTran1410@gmail.com>
2020-05-20 15:45:34 -0400

    Add Language Selection

--- code-spot-client/src/app/app.module.ts
+++ code-spot-client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,8 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     MonacoEditorModule.forRoot()
   ],
   providers: [],
--- code-spot-client/src/app/home/home.component.ts
+++ code-spot-client/src/app/home/home.component.ts
@@ -1,17 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit{
   constructor() { }
+
+  selectedLang;
+  languageForm = new FormGroup({
+    language: new FormControl(
+      "cpp",
+      Validators.compose([Validators.required])
+    )
+  });
+
+  ngOnInit() {
+    this.selectedLang = "cpp";
+ }
+
+  @Input() languages = [
+    { name: "C++", value: "cpp" },
+    { name: "C#", value: "csharp" },
+    { name: "Java", value: "java" },
+    { name: "Python", value: "python" },
+    { name: "JavaScript", value: "javascript" }
+  ]
+
+  editorCplusplus = { theme: "vs-dark", language: "cpp" };
+  editorCsharp = { theme: "vs-dark", language: "csharp" };
+  editorJava = { theme: "vs-dark", language: "java" };
+  editorJS = { theme: "vs-dark", language: "javascript" };
+  editorPython = { theme: "vs-dark", language: "python" };
 
   editorOptions = { theme: 'vs-dark', language: 'javascript' };
   code: string = 'function x() {\nconsole.log("Hello world!");\n}';
 
-  ngOnInit() {
+  onLanguageChange(res) {
+    this.selectedLang = res.slice(res.indexOf(':') + 2);;
+    console.log(this.selectedLang);
   }
-
 }
