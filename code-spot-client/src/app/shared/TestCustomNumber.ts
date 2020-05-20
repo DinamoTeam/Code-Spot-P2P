f741ac10870fe6752de8a50e4de02a00c91ceae8
Anh Minh Tran <anhminhtran235@gmail.com>
2020-05-20 16:31:24 -0700

    Add generateLessThan autotest

--- code-spot-client/src/app/shared/TestCustomNumber.ts
+++ code-spot-client/src/app/shared/TestCustomNumber.ts
@@ -29,12 +29,13 @@ function testSubtractGreaterThanBase10(numberOfTestCases: number): void {
     let actualResult = CustomNumber.subtractGreaterThan(customNumberA, customNumberB);
 
     if (actualResult.arr[0] == 0) {
-      console.error('testSubtractGreaterThanBase10: Error: 0 at beginning!');
+      console.error('ERROR: testSubtractGreaterThanBase10: Error: 0 at beginning!');
       numError++;
     }
 
     if (resultShouldBe !== parseInt(actualResult.toString())) {
-      console.error(a + ' - ' + b + ' = ' + resultShouldBe + ', actualResult: ' + actualResult.toString());
+      console.error('ERROR: ' + a + ' - ' + b + ' = ' + resultShouldBe +
+                    ', actualResult: ' + actualResult.toString());
       numError++;
     }
   }
@@ -57,12 +58,13 @@ function testAddBase10(numberOfTestCases: number): void {
     let actualResult = CustomNumber.add(customNumberA, customNumberB);
 
     if (actualResult.arr[0] == 0) {
-      console.error('testAddBase10: Error: 0 at beginning!');
+      console.error('ERROR: testAddBase10: Error: 0 at beginning!');
       numError++;
     }
 
     if (resultShouldBe !== parseInt(actualResult.toString())) {
-      console.error(a + ' + ' + b + ' = ' + resultShouldBe + ', actualResult: ' + actualResult.toString());
+      console.error('ERROR: ' + a + ' + ' + b + ' = ' + resultShouldBe +
+                    ', actualResult: ' + actualResult.toString());
       numError++;
     }
   }
@@ -71,5 +73,36 @@ function testAddBase10(numberOfTestCases: number): void {
     + ' test cases, ' + numError + ' errors found');
 }
 
+// Will console.error() an error if there is any
+function testGenerateLessThan(numberOfTestCases: number): void {
+  const MAX_NUM_DIGIT = 1;
+  const NUM_TEST_PER_NUMBER = 100;
+  let numError = 0;
+  for (let i = 0; i < numberOfTestCases; i++) {
+    const numDigits = Math.floor(Math.random() * MAX_NUM_DIGIT) + 1;
+    const arr: number[] = [];
+    for (let j = 0; j < numDigits; j++) {
+      arr[j] = Math.floor(Math.random() * CustomNumber.BASE);
+    }
+    let bigger = new CustomNumber(arr);
+    // bigger should never be 0, since 'nothing' is less than 0
+    bigger = CustomNumber.add(bigger, new CustomNumber([1]));
+    for (let j = 0; j < NUM_TEST_PER_NUMBER; j++) {
+      const smaller = CustomNumber.generateLessThan(bigger);
+      if (smaller.compareTo(bigger) >= 0) { // If smaller is actually not smaller
+        console.error('ERROR: Test generateLessThan, smaller of ' +
+          bigger.toString() + ' is ' + smaller.toString());
+        numError++;
+      }
+    }
+  }
+  console.log('Tested CustomNumer.generateLessThan() with ' + numberOfTestCases
+    + ' test cases, ' + numError + ' errors found');
+}
+
+// NOTE: check BASE in CustomNumber before run test
+
 testSubtractGreaterThanBase10(100000);
 testAddBase10(100000);
+testGenerateLessThan(1000); // Choose small numberOfTestCases since each test case will be tested 100 times
+
