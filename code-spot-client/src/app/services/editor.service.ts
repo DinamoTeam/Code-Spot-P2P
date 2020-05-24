c852f0545f8e24cf66f74b6e67668eb1b025cddf
Heidi Tran <HeidiTran1410@gmail.com>
2020-05-23 21:26:46 -0400

    Add handleLocalInsert

--- code-spot-client/src/app/services/editor.service.ts
+++ code-spot-client/src/app/services/editor.service.ts
@@ -8,28 +8,56 @@ import { CustomNumber } from '../shared/CustomNumber';
 export class EditorService {
 
   arr: CRDT[];
-  static curClock: number = 0;
+  curClock: number = 0;
 
   constructor() {
     this.arr[0] = new CRDT(
       "_beg",
       new CRDTId(
         [new Identifier(1, 0)],
-        0
+        this.curClock++
       ));
+    
     this.arr[1] = new CRDT(
       "_end",
       new CRDTId(
         [new Identifier(CustomNumber.BASE, 0)],
-        1
+        this.curClock++
       ));
   }
 
   handleLocalInsert(ch: string, index: number): void {
+    const siteIdTemp = 1;
+
     index += 1; // because we have beg limit
     const crdtIdBefore = this.arr[index - 1].id;
     const crdtIdAfter = this.arr[index].id;
 
-    const crdtIdBetween = CRDTId.generatePositionBetween(crdtIdBefore, crdtIdAfter, )
+    const crdtIdBetween = CRDTId.generatePositionBetween(crdtIdBefore, crdtIdAfter, siteIdTemp, this.curClock++);
+
+    const crdtBetween = new CRDT(ch, crdtIdBetween);
+
+    this.insertCrdtToSortedCrdtArr(crdtBetween, this.arr);
+    this.broadCastInsert(crdtBetween);
+  }
+
+  handleLocalRemove(ch: string, index: number): void {
+
+  }
+
+  insertCrdtToSortedCrdtArr(crdt: CRDT, crdtArr: CRDT[]): void {
+
+  }
+
+  broadCastInsert(crdt: CRDT): void {
+    
+  }
+
+  removeCrdtFromSortedCrdtArr(crdt: CRDT, crdtArr: CRDT[]): void {
+
+  }
+
+  broadcastRemove(crdt: CRDT): void {
+
   }
 }
