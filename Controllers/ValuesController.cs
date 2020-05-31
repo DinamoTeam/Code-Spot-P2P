8f0e60db3b2ee2512fcb9e3aadf31534d2a51eff
Heidi Tran <HeidiTran1410@gmail.com>
2020-05-30 20:37:25 -0400

    Restored ValuesController.cs for Dev purpose

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// DO NOT DELETE! launchSettings.json has specify this file as launchUrl for DEVELOPMENT
+namespace WebCRM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "The system is ready to serve you." };
+        }
+    }
+}
