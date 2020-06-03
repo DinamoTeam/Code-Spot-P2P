a468faca7cc4bb22f172c818122f911aeb96279a
Anh Minh Tran <anhminhtran235@gmail.com>
2020-06-02 19:55:35 -0700

    BalancedBST: Add AVL implementation. DONE

--- code-spot-client/src/app/shared/BalancedBST.ts
+++ code-spot-client/src/app/shared/BalancedBST.ts
@@ -31,7 +31,9 @@ export class BalancedBST<T extends IsObject> {
         if (node.left === null) {
           node.left = new Node<T>(data, node);
           this.incrementAllAncestorsSize(node.left);
-          return this.getIndex(node.left.data);
+          const indexToReturn = this.getIndex(node.left.data);
+          this.goUpRebalanceAndUpdateHeightBFAndSubtreeSize(node);
+          return indexToReturn;
         } else {
           node = node.left;
         }
@@ -40,7 +42,9 @@ export class BalancedBST<T extends IsObject> {
         if (node.right === null) {
           node.right = new Node<T>(data, node);
           this.incrementAllAncestorsSize(node.right);
-          return this.getIndex(node.right.data);
+          const indexToReturn = this.getIndex(node.right.data);
+          this.goUpRebalanceAndUpdateHeightBFAndSubtreeSize(node);
+          return indexToReturn;
         } else {
           node = node.right;
         }
@@ -73,6 +77,7 @@ export class BalancedBST<T extends IsObject> {
         } else {
           parent.right = null;
         }
+        this.goUpRebalanceAndUpdateHeightBFAndSubtreeSize(parent);
       }
     } else if (nodeToRemove.left && nodeToRemove.right) {
       // If has 2 children
@@ -102,6 +107,7 @@ export class BalancedBST<T extends IsObject> {
             parent.right = nodeToRemove.left;
             nodeToRemove.left.parent = parent;
           }
+          this.goUpRebalanceAndUpdateHeightBFAndSubtreeSize(parent);
         }
       } else {
         // It has right child
@@ -118,6 +124,7 @@ export class BalancedBST<T extends IsObject> {
             parent.right = nodeToRemove.right;
             nodeToRemove.right.parent = parent;
           }
+          this.goUpRebalanceAndUpdateHeightBFAndSubtreeSize(parent);
         }
       }
     }
@@ -150,13 +157,13 @@ export class BalancedBST<T extends IsObject> {
     while (node) {
       if (data.compareTo(node.data) === 0) {
         // subtree of a leaf has size 0. subtree of null has size -1
-        const leftSubtreeSize = (node.left) ? node.left.subtreeSize : -1;
+        const leftSubtreeSize = node.left ? node.left.subtreeSize : -1;
         index += leftSubtreeSize + 1;
         return index;
-      } else if(data.compareTo(node.data) < 0) {
+      } else if (data.compareTo(node.data) < 0) {
         node = node.left;
       } else {
-        const leftSubtreeSize = (node.left) ? node.left.subtreeSize : -1;
+        const leftSubtreeSize = node.left ? node.left.subtreeSize : -1;
         index += leftSubtreeSize + 2;
         node = node.right;
       }
@@ -172,18 +179,146 @@ export class BalancedBST<T extends IsObject> {
 
     let node = this.root;
     while (node) {
-      const leftSubtreeSize = (node.left) ? node.left.subtreeSize : -1;
+      const leftSubtreeSize = node.left ? node.left.subtreeSize : -1;
       if (index === leftSubtreeSize + 1) {
         return node.data;
       } else if (index <= leftSubtreeSize) {
         node = node.left;
       } else {
-        index -= (leftSubtreeSize + 2);
+        index -= leftSubtreeSize + 2;
         node = node.right;
       }
     }
     // Weird. Something went wrong
     throw new Error('Something went wrong when getDataAt(index)');
+  }
+
+  goUpRebalanceAndUpdateHeightBFAndSubtreeSize(node: Node<T>): void {
+    this.updateHeightBFAndSubtreeSize(node);
+
+    const nodeIsRoot = node === this.root;
+
+    // Rotate if needed
+
+    // 1.Right heavy: Either RR or RL
+    if (node.balancedFactor === 2) {
+      // Must be RR
+      if (node.bfRight() >= 0) {
+        node = this.leftRotate(node);
+      } else {
+        // Must be RL
+        this.rightRotate(node.right);
+        node = this.leftRotate(node);
+      }
+    }
+    // 2.Left heavy: Either LL or LR
+    else if (node.balancedFactor === -2) {
+      // Must be LL
+      if (node.bfLeft() <= 0) {
+        node = this.rightRotate(node);
+      } else {
+        this.leftRotate(node.left);
+        node = this.rightRotate(node);
+      }
+    }
+
+    // If we've reached the root
+    if (nodeIsRoot) {
+      this.root = node;
+      this.updateHeightBFAndSubtreeSize(this.root);
+      return;
+    } else {
+      // Go up and fix parent node
+      this.goUpRebalanceAndUpdateHeightBFAndSubtreeSize(node.parent);
+    }
+  }
+
+  updateHeightBFAndSubtreeSize(node: Node<T>): void {
+    node.height = Math.max(node.heightLeft(), node.heightRight()) + 1;
+    node.balancedFactor = node.heightRight() - node.heightLeft();
+    node.subtreeSize = node.subtreeSizeLeft() + node.subtreeSizeRight() + 2;
+  }
+
+  leftRotate(node: Node<T>): Node<T> {
+    const nodeParent = node.parent;
+    const x = node;
+    const y = node.right;
+
+    x.right = y.left;
+    if (y.left) {
+      y.left.parent = x;
+    }
+
+    x.parent = y;
+    y.left = x;
+
+    y.parent = nodeParent;
+    if (nodeParent) {
+      if (nodeParent.left === x) {
+        nodeParent.left = y;
+      } else {
+        nodeParent.right = y;
+      }
+    }
+
+    this.updateHeightBFAndSubtreeSize(x);
+    this.updateHeightBFAndSubtreeSize(y);
+
+    return y;
+  }
+
+  rightRotate(node: Node<T>): Node<T> {
+    const nodeParent = node.parent;
+    const x = node;
+    const y = node.left;
+
+    x.left = y.right;
+    if (y.right) {
+      y.right.parent = x;
+    }
+
+    y.right = x;
+    x.parent = y;
+
+    y.parent = nodeParent;
+    if (nodeParent) {
+      if (nodeParent.left === x) {
+        nodeParent.left = y;
+      } else {
+        nodeParent.right = y;
+      }
+    }
+
+    this.updateHeightBFAndSubtreeSize(x);
+    this.updateHeightBFAndSubtreeSize(y);
+
+    return y;
+  }
+
+  isBalance(): boolean {
+    const maximumHeight = Math.ceil(Math.log2(this.size)) + 1;
+    return this.getHeight() <= maximumHeight;
+  }
+
+  // Note: I can simply get height from this.root.height,
+  // but I want to calculate everything again to see if they confirm each other
+  getHeight(): number {
+    if (this.isEmpty()) {
+      return 0;
+    }
+    return this.findMaximumDepth(this.root);
+  }
+
+  private findMaximumDepth(node: Node<T>): number {
+    if (!node) {
+      return -1;
+    }
+    return (
+      Math.max(
+        this.findMaximumDepth(node.left),
+        this.findMaximumDepth(node.right)
+      ) + 1
+    );
   }
 
   getSize(): number {
@@ -267,15 +402,40 @@ class Node<T> {
   left: Node<T>;
   right: Node<T>;
   parent: Node<T>;
-  constructor(
-    data: T,
-    parent: Node<T>
-  ) {
+  height: number;
+  balancedFactor: number;
+  constructor(data: T, parent: Node<T>) {
     this.data = data;
     this.left = null;
     this.right = null;
     this.parent = parent;
     this.subtreeSize = 0;
+    this.height = 0;
+    this.balancedFactor = 0;
+  }
+
+  heightLeft(): number {
+    return this.left ? this.left.height : -1;
+  }
+
+  heightRight(): number {
+    return this.right ? this.right.height : -1;
+  }
+
+  bfLeft(): number {
+    return this.left ? this.left.balancedFactor : 0;
+  }
+
+  bfRight(): number {
+    return this.right ? this.right.balancedFactor : 0;
+  }
+
+  subtreeSizeLeft(): number {
+    return this.left ? this.left.subtreeSize : -1;
+  }
+
+  subtreeSizeRight(): number {
+    return this.right ? this.right.subtreeSize : -1;
   }
 }
 
