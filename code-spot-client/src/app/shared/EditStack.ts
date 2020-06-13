2802023366d4623b07c09e670b52c8e5720a8e94
Anh Minh Tran <anhminhtran235@gmail.com>
2020-06-12 17:59:12 -0700

    Add EditStack class

---
+++ code-spot-client/src/app/shared/EditStack.ts
@@ -0,0 +1,46 @@
+import { CRDT } from './CRDT';
+
+export class EditStack {
+  arr: Array<Edit>;
+  constructor() {
+    this.arr = new Array<Edit>();
+  }
+  push(edit: Edit): void {
+    this.arr.push(edit);
+  }
+  pop(): Edit {
+    if (this.arr.length === 0) {
+      return null;
+    }
+    return this.arr.pop();
+  }
+  popTillStop(): Edit[] {
+    if (this.arr.length === 0) {
+      return null;
+    }
+    const editArr: Edit[] = [];
+    while (this.arr.length > 0 && !this.arr[this.arr.length - 1].isStop) {
+      editArr.push(this.arr.pop());
+    }
+    return editArr;
+  }
+  isEmpty(): boolean {
+    return this.arr.length === 0;
+  }
+}
+
+export const enum EditType {
+  Remove = 0,
+  Insert = 1,
+}
+
+export class Edit {
+  constructor(edit: Array<CRDT>, editType: EditType, isStop: boolean) {
+    this.edit = edit;
+    this.editType = editType;
+    this.isStop = isStop;
+  }
+  edit: Array<CRDT>;
+  editType: EditType;
+  isStop: boolean;
+}
