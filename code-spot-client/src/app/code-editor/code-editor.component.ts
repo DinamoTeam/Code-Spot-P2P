25f3081233af0219dfbe4a39092a4e9574f7f376
Anh Minh Tran <anhminhtran235@gmail.com>
2020-07-04 12:37:07 -0700

    Add console.log(), also: log out error when observable emit event

--- code-spot-client/src/app/code-editor/code-editor.component.ts
+++ code-spot-client/src/app/code-editor/code-editor.component.ts
@@ -135,27 +135,36 @@ export class CodeEditorComponent implements OnInit {
 
   subscribeToPeerServiceEvents(): void {
     this.peerService.getRemoteInsertObservable().subscribe((crdts) => {
+      console.log('Catch a remote insert event');
       this.editorService.handleRemoteRangeInsert(
         this.editorTextModel,
         this.auxEditorTextModel,
         crdts
       );
+    }, error => {
+      console.error(error);
     });
 
     this.peerService.getRemoteRemoveObservable().subscribe((crdts) => {
+      console.log('Catch a remote remote event');
       this.editorService.handleRemoteRangeRemove(
         this.editorTextModel,
         this.auxEditorTextModel,
         crdts
       );
+    }, error => {
+      console.log(error);
     });
 
     this.peerService.getAllMessagesObservable().subscribe((crdts) => {
+      console.log('Catch all messages event');
       this.editorService.handleAllMessages(
         this.editorTextModel,
         this.auxEditorTextModel,
         crdts
       );
+    }, error => {
+      console.log(error);
     });
   }
 
