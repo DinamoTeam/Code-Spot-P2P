5ce2fe42173b7188ecf93eee6afee85ec3516b22
Heidi Tran <HeidiTran1410@gmail.com>
2020-07-17 16:56:27 -0400

    Add feedback form

--- code-spot-client/src/app/contact/contact.component.ts
+++ code-spot-client/src/app/contact/contact.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  contactForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    });
   }
 
+  ngOnInit() {}
+
+  onSubmit(form) {
+    console.log(form);
+    this.contactForm.reset();
+
+    if (form['message'] != '') {
+      console.log(JSON.stringify(form));
+      // TODO: send to backend to send email
+    }
+  }
 }
