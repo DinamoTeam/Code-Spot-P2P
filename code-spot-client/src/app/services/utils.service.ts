b456d08f61044be3b34dca43933a119bd0c73925
Heidi Tran <HeidiTran1410@gmail.com>
2020-07-17 21:35:49 -0400

    Add C# code to send email

---
+++ code-spot-client/src/app/services/utils.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { retry, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+  private apiURL = environment.apiUrl + 'Utilities/';
+  private HTTP_OPTIONS = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  constructor(private http: HttpClient) { }
+
+  sendEmail(email) {
+    return this.http.post(this.apiURL + 'SendEmail', JSON.stringify(email), this.HTTP_OPTIONS).pipe(
+      retry(1),
+      catchError(this.handleError))
+  }
+
+  // Error handling
+  private handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = 'Error from serve!';
+    }
+    window.alert(errorMessage);
+    return throwError(error);
+  }
+}
