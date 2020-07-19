7788f7f2be03a84cd7488677fd0beda5f4c099e4
Heidi Tran <HeidiTran1410@gmail.com>
2020-07-18 22:55:17 -0400

    Redesign Home page UI

--- code-spot-client/src/app/home/home.component.ts
+++ code-spot-client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {  }
-  
+  ngOnInit() {}
+
+  goToEditor() {
+    this.router.navigate(['editor']);
+  }
 }
