64a4dc5c004256f5b5a32c03fcaabfbf6cb73cce
Heidi Tran <HeidiTran1410@gmail.com>
2020-07-19 23:04:48 -0400

    Fix copy shareable link

--- code-spot-client/src/app/code-editor/code-editor.component.ts
+++ code-spot-client/src/app/code-editor/code-editor.component.ts
@@ -356,7 +356,17 @@ export class CodeEditorComponent implements OnInit {
 
   showSuccessAlert: boolean = false;
   copyLink(): void {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = window.location.href;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
     document.execCommand('copy');
+    document.body.removeChild(selBox);
     this.alertifyService.success('Link copied to clip board');
   }
 
