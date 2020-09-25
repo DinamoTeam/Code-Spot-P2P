5cf038a7f719dd3f2768697e6fa9b67915d9bfba
Heidi Tran <HeidiTran1410@gmail.com>
2020-09-24 21:37:35 -0400

    Add model for StringHelper

---
+++ Models/StringHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeSpot.Models
+{
+	public class StringHelper
+	{
+		public string Val { get; set; }
+	}
+}
