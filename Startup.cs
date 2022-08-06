f4866d007a0e1dd3b36f86597e08bfef5a661667
Anh Minh Tran <anhminhtran235@gmail.com>
2022-08-06 18:21:17 -0400

    Add back UseHttpsRedirection for prod

--- Startup.cs
+++ Startup.cs
@@ -81,6 +81,7 @@ namespace CodeSpotP2P
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
             app.UseDeveloperExceptionPage();
             app.UseCors(_allowedOrigins);
